c0404c7d74c4c246e412934a0a2e492928e9899e
Malosse Romain <romain.malosse43@icloud.com>
2025-01-24 09:23:02 +0100

    style: update dishes display & remove old css

--- pages/Home.vue
+++ pages/Home.vue
@@ -3,19 +3,7 @@
 </template>
 
 <script lang="ts" setup>
-
 </script>
 
 <style>
-  div{
-    text-align : center;
-  }
-
-  .test {
-    font-size: 40px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-  }
 </style>
