e8b3f4157195253a3746f793a435bf2de0f22c3a
Mathis Béal <mathis.beal04@gmail.com>
2025-01-24 10:43:20 +0100

    feat: Bottle Viewer OK, must give ratio (0-100) about hydratation accomplishment

---
+++ components/BottleViewer.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import {reactive, watch} from "#imports";
+
+const props = defineProps({
+  value: {
+    type: Number,
+    required: true,
+  }
+});
+
+
+const styleObject = reactive({
+  background: '',
+});
+
+watch(
+  () => props.value,
+  (newValue) => {
+    const offset = 1;
+
+    let min_water, max_water;
+
+    min_water = props.value;
+    max_water = min_water + offset;
+
+    max_water = Math.min(max_water, 100);
+    min_water = Math.min(min_water, max_water - offset);
+
+    min_water = Math.max(min_water, 0);
+    max_water = Math.max(min_water + offset, max_water);
+
+    console.log(min_water, max_water);
+
+    styleObject.background = `linear-gradient(0deg, rgba(0,213,255,1) ${min_water}%, rgba(200,234,255,0) ${max_water}%)`;
+  },
+  { immediate: true }
+);
+
+</script>
+
+<template>
+  <div id="bottle-container">
+    <img src="assets/img/bottle_mask.png" alt="" :style="styleObject">
+  </div>
+</template>
+
+<style scoped>
+#bottle-container {
+  background: #cacaca;
+  width: fit-content;
+  height: fit-content;
+  margin-inline: auto;
+  margin-bottom: 5em;
+  padding: 0;
+  line-height: 0;
+}
+</style>
---
+++ pages/test-bouteille.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import BottleViewer from "~/components/BottleViewer.vue";
+import {ref} from "#imports";
+
+const ratio = ref(0);
+
+const offset = 2;
+
+const increase = () => {
+  ratio.value += offset;
+  if (ratio.value < 0) {
+    ratio.value = 0;
+  }
+};
+
+const decrease = () => {
+  ratio.value -= offset;
+  if (ratio.value > 100) {
+    ratio.value = 100;
+  }
+};
+
+</script>
+
+<template>
+  <div>
+    <BottleViewer :value="ratio"/>
+    <button @click="increase" >+</button>
+    <button @click="decrease" >-</button>
+  </div>
+</template>
+
+<style scoped>
+div {
+  width: auto;
+  height: auto;
+  text-align: center;
+}
+</style>
