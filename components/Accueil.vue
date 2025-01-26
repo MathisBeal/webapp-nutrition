48431985b3ad8eb6313e7dc38a88591465555b43
Bilal Oulahal <Bilal.OULAHAL@etu.uca.fr>
2025-01-26 18:46:35 +0100

    refactor: remove mysql2 & use prisma

--- components/Accueil.vue
+++ components/Accueil.vue
@@ -2,24 +2,23 @@
     <div class="accueil">
       <h1>Liste des Aliments</h1>
       <div class="aliment-list">
-        <div v-for="aliment in aliments" :key="aliment.alim_code" class="aliment-item">
-          <h2>{{ aliment.nom_fr }}</h2>
-          <p>{{ aliment.nom_en }}</p>
+        <div v-for="aliment in aliments" :key="aliment.ID_aliment" class="aliment-item">
+          <h2>{{ aliment.nom }}</h2>
+          <p>{{ aliment.image }}</p>
         </div>
       </div>
     </div>
   </template>
-  
+
   <script setup lang="ts">
   import { ref } from 'vue';
   import { useAsyncData } from '#app';
-  
+
   // Liste des alimetns
   const { data: aliments } = await useAsyncData('aliments', () =>
     $fetch('/api/aliment')
   );
   </script>
-  
-  
 
-  
+
+
--- server/api/grp.ts
+++
@@ -1,8 +0,0 @@
-import { defineEventHandler } from 'h3';
-import { pool } from '../db/connection';
-
-export default defineEventHandler(async () => {
-  const [rows] = await pool.query('SELECT * FROM grp');
-
-  return rows;
-});
