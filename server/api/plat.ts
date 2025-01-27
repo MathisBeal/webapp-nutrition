ea6bf7973a302dc4e883b4b68dab1a79ee811cd7
joantony <Josselin.ANTONY@etu.uca.fr>
2025-01-27 14:50:25 +0100

    Correction josse
    Correction pour récupérer prisma

--- server/api/plat.ts
+++ server/api/plat.ts
@@ -1,8 +1,8 @@
 import { defineEventHandler } from 'h3';
-import { pool } from '../db/connection';
+import { prisma } from '../db/connection';
 
 export default defineEventHandler(async () => {
-  const [rows] = await pool.query(`
+  const [rows] = await prisma.query(`
     SELECT Plats.*, Plats_Categories.nom AS nom_categorie
     FROM Plats
     JOIN Plats_Categories ON Plats.ID_categorie = Plats_Categories.ID_categorie
