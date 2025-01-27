be7c0e24bdbd6f08776db2a8978342c051035dc1
Bilal Oulahal <Bilal.OULAHAL@etu.uca.fr>
2025-01-27 22:45:13 +0100

    feat(auth): add global auth middleware

---
+++ middleware/auth.global.ts
@@ -0,0 +1,16 @@
+export default defineNuxtRouteMiddleware((to, from) => {
+  const { loggedIn } = useUserSession();
+
+  const publicRoutes = ['/', '/login', '/signup'];
+
+  if (publicRoutes.includes(to.path)) {
+    return;
+  }
+
+  if (!loggedIn.value) {
+    return navigateTo({
+      path: '/login',
+      query: { error: `You must be logged in to access this page : ${to.path}` },
+    });
+  }
+})
