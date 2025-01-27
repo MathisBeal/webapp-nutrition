469193481c5183dccbbb793b66d45c0b5ff5a6fa
joantony <Josselin.ANTONY@etu.uca.fr>
2025-01-27 14:37:32 +0100

    Afficher 5 recettes
    Modificatiosn Accueil.vue, global.css

--- components/Accueil.vue
+++ components/Accueil.vue
@@ -1,24 +1,53 @@
 <template>
-    <div class="accueil">
-      <h1>Liste des Aliments</h1>
-      <div class="aliment-list">
-        <div v-for="aliment in aliments" :key="aliment.ID_aliment" class="aliment-item">
-          <h2>{{ aliment.nom }}</h2>
-          <p>{{ aliment.image }}</p>
+  <div :class="['accueil', { 'with-nav': isNavVisible }]">
+    <h1>Plats</h1>
+    <div class="plat-list" @scroll="handleScroll" ref="platList">
+      <div v-for="plat in displayedPlats" :key="plat.ID_plat" class="plat-item">
+        <img src="/assets/img/plat.png" alt="Image du plat" class="plat-image" />
+        <div class="plat-text">
+          <h2>{{ plat.description }} - {{ plat.nom_categorie }}</h2>
+          <p>
+            <img src="/assets/img/horloge.png" alt="Icône d'horloge" class="icon-horloge" /> Durée de préparation : {{ plat.durée }}
+          </p>
         </div>
       </div>
     </div>
-  </template>
-
-  <script setup lang="ts">
-  import { ref } from 'vue';
-  import { useAsyncData } from '#app';
-
-  // Liste des alimetns
-  const { data: aliments } = await useAsyncData('aliments', () =>
-    $fetch('/api/aliment')
-  );
-  </script>
+  </div>
+</template>
 
 
 
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import { useAsyncData } from '#app';
+import { isNavVisible } from '@/composables/useNavState';
+
+const { data: plats } = await useAsyncData('plats', () => $fetch('/api/plat'));
+
+const displayedPlats = ref([]);
+const pageSize = 5; 
+let currentPage = 1;
+
+const loadPlats = () => {
+  if (!plats.value) return;
+  const start = (currentPage - 1) * pageSize;
+  const end = currentPage * pageSize;
+
+  const newPlats = plats.value.slice(start, end);
+  if (newPlats.length) {
+    displayedPlats.value.push(...newPlats);
+    currentPage++;
+  }
+};
+
+const handleScroll = (event: Event) => {
+  const target = event.target as HTMLElement;
+  if (target.scrollTop + target.clientHeight >= target.scrollHeight - 10) {
+    loadPlats();
+  }
+};
+
+onMounted(() => {
+  loadPlats();
+});
+</script>
