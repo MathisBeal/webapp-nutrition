05b2031f89fbb4a356d445534ab70c96298d431c
Mathis Béal <mathis.beal04@gmail.com>
2025-01-27 09:47:37 +0100

    feat(pages): Pass recipe data to dedicated page

---
+++ pages/recipes/[id_recipe].vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+  const params = useRoute().params;
+  const rows = ref(null);
+
+  if (params && params.id_recipe) {
+    rows.value = await $fetch(`/api/recipes/${params.id_recipe}`);
+    console.log(rows.value)
+  }
+</script>
+
+<template>
+  <h1>
+    HEEHEE
+  </h1>
+  <RecipeDetail v-if="rows" :data_recipe="rows"/>
+  <div v-else>
+    Could not fetch recipe from database
+  </div>
+</template>
+
+<style scoped>
+
+</style>
