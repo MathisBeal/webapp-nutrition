935e3ed8e9394dfe8c828bd4e11862dca4069161
Mathis Béal <mathis.beal04@gmail.com>
2025-01-27 09:47:37 +0100

    feat(pages): Pass recipe data to dedicated page

---
+++ components/RecipeDetail.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+defineProps({
+  data_recipe: {
+    type: Array,
+    required: true,
+  },
+});
+
+
+
+</script>
+
+<template>
+  <div class="container">
+
+  <h2>
+    YEET
+  </h2>
+  <div v-for="(item, index) in data_recipe" :key="index">
+    <!-- Display data from data_recipe -->
+    <p>{{ item }}</p>
+  </div>
+  </div>
+</template>
+
+
+<style scoped>
+.container {
+  margin-inline: 11em;
+  background: red;
+}
+
+</style>
