810236ccf53b7e2e338475f467077455565d920d
Mathis Béal <mathis.beal04@gmail.com>
2025-01-28 15:21:26 +0100

    feat(home page): Redirect to recipe page when click on div

--- pages/recipes/index.vue
+++ pages/recipes/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <button v-for="(recipe, index) in recipes" :key="recipe.ID_plat" @click="goToRecipe(recipe.ID_plat)">
-      {{ recipe.description }}
-    </button>
+    <div class="recipe" v-for="(recipe, index) in recipes" :key="recipe.ID_plat" @click="goToRecipe(recipe.ID_plat)">
+      <p>{{ recipe.description }}</p>
+    </div>
   </div>
 </template>
 
@@ -16,14 +16,30 @@ function goToRecipe(page: number) {
 
 <style scoped>
 div {
-  margin: 10%;
+  margin: 5em 0 0 0;
   display: flex;
   flex-direction: column;
   align-items: center;
 }
 
-button {
-  width: fit-content;
-  margin: 0.3em auto;
+.recipe {
+  width: 40%;
+  padding: 0.4em;
+  margin: 0.4em auto;
+  border-radius: 10px;
+  box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.7);
+  background: #f3f3f3;
+  font-size: 1.5em;
+  align-items: center;
+  align-content: center;
+}
+
+.recipe:hover {
+  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.9);
+  background: #e6e6e6;
+}
+
+p {
+  margin: 0;
 }
 </style>
