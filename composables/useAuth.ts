12163ec819e0149f462ee200c38118098ebd4287
Malosse Romain <romain.malosse43@icloud.com>
2025-01-29 09:13:30 +0100

    refactor: remove unused imports

--- composables/useAuth.ts
+++ composables/useAuth.ts
@@ -1,9 +1,5 @@
-import { ref } from 'vue'
-
-// Variable réactive partagée
 export const isAuthenticated = ref(false)
 
-// Fonction pour modifier la valeur de `isAuthenticated`
 export const setAuthenticationStatus = (status: boolean) => {
   isAuthenticated.value = status
 }
