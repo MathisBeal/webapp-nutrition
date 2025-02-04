763176963f6e17755810a40db4cf297bd581c3a7
Bilal Oulahal <Bilal.OULAHAL@etu.uca.fr>
2025-02-04 20:19:12 +0100

    chore: update ESLint config and npm script + format Nuxt config and package.json

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,4 +3,9 @@ import withNuxt from "./.nuxt/eslint.config.mjs";
 
 export default withNuxt({
   files: ["**/*.ts", "**/*.tsx", "**/*.vue"],
+
+  /* Custom rules */
+  rules: {
+    "vue/multi-word-component-names": "off"
+  }
 });
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,20 @@
-import {defineNuxtConfig} from 'nuxt/config';
+import { defineNuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
-  devtools: {enabled: true},
+  devtools: { enabled: true },
 
   typescript: {
     typeCheck: true,
   },
 
-  plugins: ['~/plugins/vue-notification.ts'],
+  plugins: ["~/plugins/vue-notification.ts"],
 
   modules: [
     "@nuxt/test-utils/module",
     "@nuxt/eslint",
-    "nuxt-auth-utils",  // Docs : https://nuxt.com/modules/auth-utils
+    "nuxt-auth-utils", // Docs : https://nuxt.com/modules/auth-utils
   ],
 
-  css: ['@/assets/styles/global.css'],
+  css: ["@/assets/styles/global.css"],
 });
